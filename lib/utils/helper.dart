48b43c2c5158c33fb7b030f55474fb767e6b6865
Mr. Bill <billachusim@gmail.com>
2022-04-03 19:33:40 +0100

    Migrated share to share_plus

--- lib/utils/helper.dart
+++ lib/utils/helper.dart
@@ -6,8 +6,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
-import 'package:share/share.dart';
-
+import 'package:share_plus/share_plus.dart';
 import 'color.dart';
 
 /// Converts timeStamp from firebase
