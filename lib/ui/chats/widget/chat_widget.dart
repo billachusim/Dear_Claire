86770e9a9a1c95e6a664f97530eafcd3b02cdad7
JUEsoft <emmanueljunior433@gmail.com>
2022-05-27 18:11:01 +0100

    ChatWidget

--- lib/ui/chats/widget/chat_widget.dart
+++ lib/ui/chats/widget/chat_widget.dart
@@ -4,24 +4,37 @@ import 'package:dear_claire/services/user_model.dart';
 import 'package:dear_claire/ui/chats/data/chatroompodo.dart';
 import 'package:dear_claire/ui/chats/data/chats.dart';
 import 'package:dear_claire/ui/chats/sub_chat_screen.dart';
+import 'package:dear_claire/ui/featured/model/session.dart';
 import 'package:dear_claire/ui/routes/page_router_animation.dart';
 import 'package:dear_claire/ui/featured/model/comment_session_model.dart';
+import 'package:dear_claire/ui/visited_user_ego_page/visited_user_ego_page.dart';
 import 'package:dear_claire/utils/color.dart';
 import 'package:dear_claire/utils/constant.dart';
 import 'package:dear_claire/utils/enums.dart';
 import 'package:dear_claire/utils/helper.dart';
 import 'package:dear_claire/widgets/share_button.dart';
 import 'package:dear_claire/widgets/thanks_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class ChatWidget extends StatelessWidget {
+
+  UserModel userModel = UserModel();
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  getUser() async {
+    userModel = await firebaseServices.getUserInfo();
+  }
+
   String? documentID;
   ChatModel? chatModel;
   ChatRoomPodo? chatRoomPodo;
 
   /// use this bool value to determine when a chat is sub chat or not
   bool? isSubChat;
+  late String visitedUsersID;
+  late String visitedEgoName;
 
   ChatWidget(
       {Key? key,
@@ -53,54 +66,82 @@ class ChatWidget extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    CachedNetworkImage(
-                        width: 35,
-                        height: 35,
-                        imageUrl: _user!.avatarUrl ?? '',
-                        imageBuilder: (context, imageProvider) => Container(
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: imageProvider,
-                                  fit: BoxFit.fill,
+                    GestureDetector(
+                      onTap: () {
+                        visitedUsersID = _user?.userId ?? '';
+                        visitedEgoName = _user?.nickname ?? 'Chatter';
+                        String thisEgoName = visitedEgoName;
+                        String thisUser = visitedUsersID;
+                        PageRouter.gotoWidget(
+                            VisitedUserEgoProfilePage(
+                                visitedUsersID: thisUser,
+                                visitedEgoName: thisEgoName),
+                            context);
+                        print("Visited User ID::: $visitedUsersID");
+                      },
+                      child: CachedNetworkImage(
+                          width: 35,
+                          height: 35,
+                          imageUrl: _user!.avatarUrl ?? '',
+                          imageBuilder: (context, imageProvider) => Container(
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: imageProvider,
+                                    fit: BoxFit.fill,
+                                  ),
                                 ),
                               ),
-                            ),
-                        placeholder: (context, url) =>
-                            Center(child: CircularProgressIndicator()),
-                        errorWidget: (context, url, error) => Image.asset(
-                              "assets/images/brown_boy_mask.png",
-                              width: 35,
-                              height: 35,
-                            ) //Icon(Icons.error),
-                        ),
+                          placeholder: (context, url) =>
+                              Center(child: CircularProgressIndicator()),
+                          errorWidget: (context, url, error) => Image.asset(
+                                "assets/images/brown_boy_mask.png",
+                                width: 35,
+                                height: 35,
+                              ) //Icon(Icons.error),
+                          ),
+                    ),
                     SizedBox(
                       width: 4,
                     ),
                     Expanded(
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(_user.nickname ?? '',
-                              textAlign: TextAlign.start,
-                              maxLines: 1,
-                              style: GoogleFonts.lato(
-                                  fontSize: 13.0,
-                                  color: Pallet.colorBlack,
-                                  fontWeight: FontWeight.w800)),
-                          SizedBox(
-                            height: 2,
-                          ),
-                          Text(
-                              timeConverter(chatModel!.timeCreated!,
-                                  time: TimeConverterEnum.Comment),
-                              textAlign: TextAlign.start,
-                              maxLines: 1,
-                              style: GoogleFonts.lato(
-                                  fontSize: 11.0,
-                                  color: Pallet.colorGrey,
-                                  fontWeight: FontWeight.normal)),
-                        ],
+                      child: GestureDetector(
+                        onTap: () {
+                          visitedUsersID = _user.userId ?? '';
+                          visitedEgoName = _user.nickname ?? 'Chatter';
+                          String thisEgoName = visitedEgoName;
+                          String thisUser = visitedUsersID;
+                          PageRouter.gotoWidget(
+                              VisitedUserEgoProfilePage(
+                                  visitedUsersID: thisUser,
+                                  visitedEgoName: thisEgoName),
+                              context);
+                          print("Visited User ID::: $visitedUsersID");
+                        },
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(_user.nickname ?? '',
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: GoogleFonts.lato(
+                                    fontSize: 13.0,
+                                    color: Pallet.colorBlack,
+                                    fontWeight: FontWeight.w800)),
+                            SizedBox(
+                              height: 2,
+                            ),
+                            Text(
+                                timeConverter(chatModel!.timeCreated!,
+                                    time: TimeConverterEnum.Comment),
+                                textAlign: TextAlign.start,
+                                maxLines: 1,
+                                style: GoogleFonts.lato(
+                                    fontSize: 11.0,
+                                    color: Pallet.colorGrey,
+                                    fontWeight: FontWeight.normal)),
+                          ],
+                        ),
                       ),
                     ),
                   ],
