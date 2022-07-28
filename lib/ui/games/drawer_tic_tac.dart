950f031753f4cff6eb2c5611390c90eff45e4269
Mr. Bill <billachusim@gmail.com>
2022-07-28 08:36:52 +0100

    Get 10 loves for winning Claire on tic tac toe

--- lib/ui/games/drawer_tic_tac.dart
+++ lib/ui/games/drawer_tic_tac.dart
@@ -1,9 +1,16 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:convert';
 import 'package:webview_flutter/webview_flutter.dart';
+
+import '../../services/firebase_services.dart';
+import '../../utils/color.dart';
+import '../routes/routes.dart';
 
 class TicTacToe extends StatefulWidget {
   const TicTacToe({Key? key}) : super(key: key);
@@ -17,6 +24,65 @@ class TicTacToe extends StatefulWidget {
 class _TicTacToeState extends State<TicTacToe> {
   late WebViewController _webViewController;
   String filePath = 'assets/web_games/tictactoe/index2.html';
+  bool isWon = false;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+
+  final AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      importance: Importance.high,
+      playSound: true);
+
+  NotificationDetails? _notificationDetails() {
+    return NotificationDetails(
+        android: AndroidNotificationDetails(
+            channel.id, channel.name,
+            color: Pallet.colorPrimary,
+            playSound: true,
+            icon: '@drawable/claire_icon',
+            enableLights: true,
+            enableVibration: true,
+            showWhen: true,
+            channelShowBadge: true),
+        iOS: IOSNotificationDetails(
+            presentAlert: true,
+            presentBadge: true,
+            presentSound: true));
+  }
+
+  /// Increase total love count when user wins on tic tac toe.
+
+  Future<void> incrementTotalLoveCount() async {
+    FirebaseFirestore.instance.collection('users').doc(currentUser?.uid).set(
+      {
+        'totalLoveCount': FieldValue.increment(10),
+      },
+      SetOptions(merge: true),
+    );
+    logger.d('Successfully increased total love count');
+    print('Session Count is: $FieldValue');
+
+    flutterLocalNotificationsPlugin.show(0, 'ClaireLove Wallet',
+        "Nice, you won me on Tic tac toe. 10 Loves for you.", _notificationDetails());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +90,17 @@ class _TicTacToeState extends State<TicTacToe> {
       body: WebView(
         initialUrl: '',
         javascriptMode: JavascriptMode.unrestricted,
+        javascriptChannels: Set.from([
+          JavascriptChannel(
+              name: 'Score',
+              onMessageReceived: (JavascriptMessage message){
+                Fluttertoast.showToast(msg: message.message);
+                if (message.message != null) {
+                  isWon = true;
+                  incrementTotalLoveCount();
+                }
+              })
+        ]),
         onWebViewCreated: (WebViewController webViewController) {
           _webViewController = webViewController;
           _loadHtmlFromAssets();
--- lib/ui/games/tic_tac_toe.dart
+++ lib/ui/games/tic_tac_toe.dart
@@ -1,9 +1,17 @@
 // ignore_for_file: deprecated_member_use
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'dart:convert';
 import 'package:webview_flutter/webview_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../services/firebase_services.dart';
+import '../../utils/color.dart';
+import '../routes/routes.dart';
 
 class TicTacToe extends StatefulWidget {
   const TicTacToe({Key? key}) : super(key: key);
@@ -17,13 +25,94 @@ class TicTacToe extends StatefulWidget {
 class _TicTacToeState extends State<TicTacToe> {
   late WebViewController _webViewController;
   String filePath = 'assets/web_games/tictactoe/index.html';
+  bool isWon = false;
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
+
+
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+
+
+  final AndroidNotificationChannel channel = AndroidNotificationChannel(
+      'high_importance_channel', // id
+      'High Importance Notifications', // title
+      importance: Importance.high,
+      playSound: true);
+
+  NotificationDetails? _notificationDetails() {
+    return NotificationDetails(
+        android: AndroidNotificationDetails(
+            channel.id, channel.name,
+            color: Pallet.colorPrimary,
+            playSound: true,
+            icon: '@drawable/claire_icon',
+            enableLights: true,
+            enableVibration: true,
+            showWhen: true,
+            channelShowBadge: true),
+        iOS: IOSNotificationDetails(
+            presentAlert: true,
+            presentBadge: true,
+            presentSound: true));
+  }
+
+  /// Increase total love count when user wins on tic tac toe.
+
+  Future<void> incrementTotalLoveCount() async {
+    FirebaseFirestore.instance.collection('users').doc(currentUser?.uid).set(
+      {
+        'totalLoveCount': FieldValue.increment(10),
+      },
+      SetOptions(merge: true),
+    );
+    logger.d('Successfully increased total love count');
+    print('Session Count is: $FieldValue');
+
+    flutterLocalNotificationsPlugin.show(0, 'ClaireLove Wallet',
+        "Nice, you won me on Tic tac toe. 10 Loves for you.", _notificationDetails());
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Pallet.colorPrimary,
+        centerTitle: false,
+        title: Text('Claire Games',
+            textAlign: TextAlign.start,
+            maxLines: 1,
+            style: GoogleFonts.lato(
+                fontSize: 24.0,
+                color: Pallet.colorWhite,
+                fontWeight: FontWeight.w600)),
+      ),
       body: WebView(
         initialUrl: '',
         javascriptMode: JavascriptMode.unrestricted,
+        javascriptChannels: Set.from([
+          JavascriptChannel(
+              name: 'Score',
+              onMessageReceived: (JavascriptMessage message){
+              Fluttertoast.showToast(msg: message.message);
+              if (message.message != null) {
+                isWon = true;
+                incrementTotalLoveCount();
+              }
+      })
+        ]),
         onWebViewCreated: (WebViewController webViewController) {
           _webViewController = webViewController;
           _loadHtmlFromAssets();
