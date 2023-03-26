ed4344b75cca9ebcf584dca3379ed3f7f8bbf193
Mr. Bill <billachusim@gmail.com>
2023-03-27 00:06:10 +0100

    iOS build working fine but still no ATT dialog. Waiting to test on real device.

--- ios/AppDelegate.swift
+++ ios/AppDelegate.swift
@@ -1,104 +1,91 @@
-// Copyright 2014 The Flutter Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import UIKit
 import Flutter
 import Firebase
 import FirebaseMessaging
-import workmanager
 import AppTrackingTransparency
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate, MessagingDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-  func applicationDidBecomeActive(_ application: UIApplication) {
-      class MyViewController: UIViewController {
-
-        override func viewDidAppear(_ animated: Bool) {
-          super.viewDidAppear(animated)
-
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-            if #available(iOS 14, *) {
-                ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
-                    switch status {
-                        case .authorized:
-                            print("enable tracking")
-                        case .denied:
-                            print("disable tracking")
-                        default:
-                            print("disable tracking")
-                    }
-                })
-            }
-          }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        FirebaseApp.configure()
+        Messaging.messaging().delegate = self
+        GeneratedPluginRegistrant.register(with: self)
+        
+        if #available(iOS 14, *) {
+            ATTrackingManager.requestTrackingAuthorization(completionHandler: { status in
+                switch status {
+                case .authorized:
+                    print("enable tracking")
+                case .denied:
+                    print("disable tracking")
+                default:
+                    print("disable tracking")
+                }
+            })
         }
-      }
-  }
-    FirebaseApp.configure()
-    Messaging.messaging().delegate = self
-    GeneratedPluginRegistrant.register(with: self)
-      if #available(iOS 10.0, *) {
-        // For iOS 10 display notification (sent via APNS)
-        UNUserNotificationCenter.current().delegate = self
+        
+        if #available(iOS 10.0, *) {
+            // For iOS 10 display notification (sent via APNS)
+            UNUserNotificationCenter.current().delegate = self
 
-        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
-        UNUserNotificationCenter.current().requestAuthorization(
-          options: authOptions,
-          completionHandler: { _, _ in }
-        )
-      } else {
-        let settings: UIUserNotificationSettings =
-          UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
-        application.registerUserNotificationSettings(settings)
-      }
-    application.registerForRemoteNotifications()
-    UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+            let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+            UNUserNotificationCenter.current().requestAuthorization(
+                options: authOptions,
+                completionHandler: { _, _ in }
+            )
+        } else {
+            let settings: UIUserNotificationSettings =
+                UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
+        application.registerForRemoteNotifications()
+        UIApplication.shared.setMinimumBackgroundFetchInterval(TimeInterval(60*15))
+        
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
     override func application(_ application: UIApplication,
-    didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+                              didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken
+        super.application(application,
+                          didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
+    }
+    
+    // Receive displayed notifications for iOS 10 devices.
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         willPresent notification: UNNotification,
+                                         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        let userInfo = notification.request.content.userInfo
 
-     Messaging.messaging().apnsToken = deviceToken
-     super.application(application,
-     didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
-     }
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        Messaging.messaging().appDidReceiveMessage(userInfo)
 
-            // Receive displayed notifications for iOS 10 devices.
-              override func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                        willPresent notification: UNNotification,
-                                        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions)
-                                          -> Void) {
-              let userInfo = notification.request.content.userInfo
+        // ...
 
-              // With swizzling disabled you must let Messaging know about the message, for Analytics
-              Messaging.messaging().appDidReceiveMessage(userInfo)
+        // Print full message.
+        print(userInfo)
 
-              // ...
+        // Change this to your preferred presentation option
+        completionHandler([[.alert, .sound]])
+    }
 
-              // Print full message.
-              print(userInfo)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         didReceive response: UNNotificationResponse,
+                                         withCompletionHandler completionHandler: @escaping () -> Void) {
+        let userInfo = response.notification.request.content.userInfo
 
-              // Change this to your preferred presentation option
-              completionHandler([[.alert, .sound]])
-            }
+        // ...
 
-            override func userNotificationCenter(_ center: UNUserNotificationCenter,
-                                        didReceive response: UNNotificationResponse,
-                                        withCompletionHandler completionHandler: @escaping () -> Void) {
-              let userInfo = response.notification.request.content.userInfo
+        // With swizzling disabled you must let Messaging know about the message, for Analytics
+        Messaging.messaging().appDidReceiveMessage(userInfo)
 
-              // ...
+        // Print full message.
+        print(userInfo)
 
-              // With swizzling disabled you must let Messaging know about the message, for Analytics
-              Messaging.messaging().appDidReceiveMessage(userInfo)
-
-              // Print full message.
-              print(userInfo)
-
-              completionHandler()
-            }
+        completionHandler()
+    }
 }
