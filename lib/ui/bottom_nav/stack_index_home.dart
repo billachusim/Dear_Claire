adccc75484c05eb48f81563734ce62ccc961b956
Mr. Bill <billachusim@gmail.com>
2023-03-29 21:34:14 +0100

    App Tracking Transparency Permission now live on iOS simulator.

--- lib/ui/bottom_nav/stack_index_home.dart
+++ lib/ui/bottom_nav/stack_index_home.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dear_claire/ui/chats/chatrooms.dart';
@@ -140,6 +141,7 @@ class _HomeDashboardPageState extends State<HomePage>
     getEgoInfo();
     _title = "Dear Claire";
     shakeDevice();
+    AppTrackingTransparency.requestTrackingAuthorization();
   }
 
   shakeDevice() async {
